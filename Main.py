1fe1b5f36e7bb6cdbf26a9f4b0d3ed7fa99c261e
kunalpatel <kunalpatel446@gmail.com>
2016-11-06 09:52:37 -0500

    working eye detection

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy
 import sys
+import cv2.cv as cv
 faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eyeCascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 cam = cv2.VideoCapture(0)
@@ -12,7 +13,10 @@ leftX = []
 leftY = []
 leftWidth = []
 leftHeight = []
-pinDownR = []
+rightX = []
+rightY = []
+rightWidth = []
+rightHeight = []
 accuracyCount = 10
 
 def rectCenter(rect):
@@ -23,12 +27,19 @@ def pupilCenter(img):
 	# inputs the median image 
 	# no outputs ATM, but draws dot on center. Later outputs coordinates of center
 	# from Hough Transforms documentation
-	img = cv2.medianBlut(img, 5)
-	cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
-	circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 1.20, param1=50, param2=30, minRadius=0, maxRadius=0)
-	cicles = numpy.uint16(np.around(circles))
-	for i in circles[0,:]:
-		cv2.circle(cimg,(i[0],i[2]),(0,255,0),2)
+	#img = cv2.medianBlur(img, 5)
+	#cv2.imshow("img", img)
+	# cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
+	# circles = cv2.HoughCircles(img, cv.CV_HOUGH_GRADIENT, 1, 20)
+	# print "circles are ", circles
+	# if circles is not None:
+	# 	circles = numpy.around(circles)
+	# 	circles = numpy.uint16(circles)
+	# 	for i in circles[0,:]:
+	# 		cv2.circle(cimg,(i[0],i[2]),(0,255,0),2)
+	#threshold = cv2.inRange(cimg,250,255)		#get the blobs
+	#contours, hierarchy = cv2.findContours(threshold,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+	#print len(contours)
 
 while True:
 	ok, img = cam.read()
@@ -43,42 +54,44 @@ while True:
 		roiR = gray[yf:yf + hf/2, xf + wf/2:xf + wf]
 		eyeL = eyeCascade.detectMultiScale(roiL)
 		for (x, y, w, h) in eyeL:
-			countL +=1
 			leftX.append(x)
 			leftY.append(y)
 			leftWidth.append(w)
 			leftHeight.append(h)
-			#if (len(pinDownL) == accuracyCount - 1):
-				#sumW= []
-				#sumH= []
-				#for i in range(len(pinDownL[0])):
-					#sumW.append(pinDownL[i][0])
-					#sumH.append(pinDownL[i][1])
 		if eyeL is not None:
 			medX = numpy.array(leftX)
 			medX = int(numpy.median(medX))
+			print "medX is ", medX
 			medY = numpy.array(leftY)
 			medY = int(numpy.median(medY))
+			print "medY is ", medY
 			medWidth = numpy.array(leftWidth)
-			medWidth = int(numpy.median(leftWidth))
+			medWidth = int(numpy.median(medWidth))
+			print "median width is ", medWidth
 			medHeight = numpy.array(leftHeight)
-			medHeight = int(numpy.median(leftHeight))
+			medHeight = int(numpy.median(medHeight))
+			print "median height is ", medHeight
 			cv2.rectangle(img, (medX + xf,medY + yf), (medX + xf + medWidth, medY + yf + medHeight), (255, 0, 0), 2)
-			#leftImg = img[(medX + xf, medY + yf), (medX + xf + medWidth, medY + yf + medHieght)]
-			#pupilCenter(lefImg)
+			leftImg = img[(medY + yf):(yf + medY + medHeight), (xf + medX):(xf + medX + medWidth)]
+			#cv2.imshow("img", leftImg)
+			#pupilCenter(leftImg)
 
 		eyeR = eyeCascade.detectMultiScale(roiR)
 		for (x, y, w, h) in eyeR:
-			cv2.rectangle(img, (x + xf + wf/2,y + yf), (x + xf + wf/2 + w, y + yf + h), (255, 0, 0), 2)
-			if (len(pinDownR) == accuracyCount - 1):
-				sumW= []
-				sumH= []
-				for i in range(len(pinDownL[0])):
-					sumW.append(pinDownR[i][0])
-					sumH.append(pinDownR[i][1])
-				medianWidth = median(sumW)
-				medianHieght = median(sumH)	
-			#if len(pindownR) == accuracyCount:
+			rightX.append(x)
+			rightY.append(y)
+			rightWidth.append(w)
+			rightHeight.append(h)
+		if eyeR is not None:
+			medX = numpy.array(rightX)
+			medX = int(numpy.median(medX))
+			medY = numpy.array(rightY)
+			medY = int(numpy.median(medY))
+			medWidth = numpy.array(rightWidth)
+			medWidth = int(numpy.median(medWidth))
+			medHeight = numpy.array(rightHeight)
+			medHeight = int(numpy.median(medHeight))
+			cv2.rectangle(img, (medX + xf + wf/2,medY + yf), (medX + xf + wf/2 + medWidth, medY + yf + medHeight), (255, 0, 0), 2)
 
 		cv2.imshow("img", img)
 	if (cv2.waitKey(30) == 27):
